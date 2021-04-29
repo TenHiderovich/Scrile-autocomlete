11dc6143dc89eda4f69a195a38dcea03728a87f9
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2021-04-29 16:48:15 +0800

    components dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Autocomplete } from './shared/Autocomplete';
+import { Autocomplete } from './components/shared/Autocomplete';
 
 function App() {
   return (
